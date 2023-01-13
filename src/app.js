73b1bfdc440ca2f0b850fc887136dad90dfd119d
Kristina <kristina.zvorykina@gmail.com>
2023-01-13 19:04:31 +0100

    fix bugs/API

--- src/app.js
+++ src/app.js
@@ -27,11 +27,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let humidutyElement = document.querySelector("#humidity");
+  let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("date");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -41,6 +44,5 @@ function displayTemperature(response) {
 
 let apiKey = "60754cb629e2cd63eeb18b5d4b8e4d52";
 let city = "New York";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
