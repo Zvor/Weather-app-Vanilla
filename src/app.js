b792bc578081f177a9514a6eb748c30759b834a2
Kristina <kristina.zvorykina@gmail.com>
2023-01-13 18:34:44 +0100

    add  date

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day} ${hours}:${minutes}`;
+
+  //calculate the date
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidutyElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "60754cb629e2cd63eeb18b5d4b8e4d52";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=${apiKey}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric";
 axios.get(apiUrl).then(displayTemperature);
