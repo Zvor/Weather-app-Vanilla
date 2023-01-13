c25cdebeb24c811d50d491172c30c0d9b0989c61
Kristina <kristina.zvorykina@gmail.com>
2023-01-13 18:26:16 +0100

    current temp

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidutyElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  cityElement.innerHTML = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "60754cb629e2cd63eeb18b5d4b8e4d52";
 let apiUrl =
-  "https://api.openweathermap.org/data/3.0/onecall?q=Zurich&appid=${apiKey}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=${apiKey}&units=metric";
 axios.get(apiUrl).then(displayTemperature);
