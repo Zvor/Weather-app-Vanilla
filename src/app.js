4c504f481b39264f50a445dd1ef6f795395fb3dc
Kristina <kristina.zvorykina@gmail.com>
2023-01-13 14:16:24 +0100

    /

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ function displayTemperature(response) {
 
 let apiKey = "60754cb629e2cd63eeb18b5d4b8e4d52";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Zurich&units=metric&appid=${apiKey}";
+  "https://api.openweathermap.org/data/3.0/onecall?q=Zurich&appid=${apiKey}&units=metric";
 axios.get(apiUrl).then(displayTemperature);
