1c580cdb2c92d6845c0b194b5abda4ebc0f75a54
Kristina <kristina.zvorykina@gmail.com>
2023-01-20 17:52:09 +0100

    Change HTML structure

--- src/app.js
+++ src/app.js
@@ -111,7 +111,7 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
-
+/* 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -132,13 +132,12 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
+*/
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 search("Lucerne");
